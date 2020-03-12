75683144254f4df3ff1dfb5343cf6b0a8678c256
liuduan <liuduan@58.com>
2020-03-12 22:59:02 +0800

    complite browserhistory

---
+++ src/react-router/hooks.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Context from "./RouterContext.js";
+import matchPath from "./matchPath.js";
+
+const useContext = React.useContext;
+
+export function useHistory() {
+  return useContext(Context).history;
+}
+
+export function useLocation() {
+  return useContext(Context).location;
+}
+
+export function useParams() {
+  const match = useContext(Context).match;
+  return match ? match.params : {};
+}
+
+export function useRouteMatch(path) {
+  return path
+    ? matchPath(useLocation().pathname, path)
+    : useContext(Context).match;
+}
--- src/react-router/withRouter.js
+++ src/react-router/withRouter.js
@@ -1,7 +1,35 @@
-/*
- * @Author: liuduan
- * @Date: 2020-03-12 18:46:57
- * @LastEditors: liuduan
- * @LastEditTime: 2020-03-12 18:46:58
- * @Description: 
+import React from "react";
+import RouterContext from "./RouterContext";
+import hoistStatics from "hoist-non-react-statics"; // 什么鬼？？有时间再看
+
+/**
+ * A public higher-order component to access the imperative API
  */
+function withRouter(Component) {
+    const displayName = `withRouter(${Component.displayName || Component.name})`;
+    const C = props => {
+        const { wrappedComponentRef, ...remainingProps } = props;
+
+        return (
+            <RouterContext.Consumer>
+                {context => {
+                    return (
+                        <Component
+                            {...remainingProps}
+                            {...context}
+                            ref={wrappedComponentRef}
+                        />
+                    );
+                }}
+            </RouterContext.Consumer>
+        );
+    };
+
+    C.displayName = displayName;
+    C.WrappedComponent = Component;
+
+
+    return hoistStatics(C, Component);
+}
+
+export default withRouter;

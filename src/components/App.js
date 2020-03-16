8658a6ebd4babd83bcd353e6ccc0889e8e96e2b4
liuduan <liuduan@58.com>
2020-03-16 08:59:27 +0800

    update components

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,9 @@ export default class App extends React.Component {
     }
     render() {
         console.log(this.props);
+        const title = /^\/a/.test(this.props.location.pathname) ? 'App' : 'Cpp'
         return <div>
-            <h1 style={{ color: "red" }}>我是app.js</h1>
+            <h1 style={{ color: "red" }}>{title}</h1>
             <p><span>当前页面历史列表中URL的数量：history.length===</span>{window.history.length} / {this.props.history.length}</p>
             <p><span>history.action===</span>{window.history.action} / {this.props.history.action}</p>
             <p><span>history.location===</span>{JSON.stringify(this.props.location)}</p>
--- src/components/Bpp.js
+++ src/components/Bpp.js
@@ -17,7 +17,7 @@ const Bpp = (props) => {
     }
 
     return <div>
-        <h1 style={{ color: "green" }}>我是bpp.js</h1>
+        <h1 style={{ color: "green" }}>Bpp</h1>
         <p><span>当前页面历史列表中URL的数量：history.length===</span>{window.history.length} / {props.history.length}</p>
         <p><span>history.action===</span>{window.history.action} / {props.history.action}</p>
         <p><span>history.location===</span>{JSON.stringify(props.history.location)}</p>

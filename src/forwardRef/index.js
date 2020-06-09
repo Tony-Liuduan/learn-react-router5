93db6b8670b5dfd061ff1a840af051fc299154de
liuduan <liuduan@58.com>
2020-06-09 16:45:31 +0800

    update

--- src/forwardRef/index.js
+++ src/forwardRef/index.js
@@ -2,13 +2,36 @@
  * @fileoverview 
  * @author liuduan
  * @Date 2020-06-09 16:21:18
- * @LastEditTime 2020-06-09 16:29:59
+ * @LastEditTime 2020-06-09 16:45:14
  */
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+/* 方式1 */
+// const FancyButton = React.forwardRef((props, ref) => {
+//     function handleClick() {
+//         console.log('handleClick', ref.current);
+//     }
+//     return (
+//         <button forwardedRef={ref} className="FancyButton" onClick={handleClick}>
+//             {props.children}
+//         </button>
+//     );
+// });
 
-const FancyButton = React.forwardRef((props, ref) => {
+// const App = () => {
+//     const ref = React.createRef();
+
+//     useEffect(() => {
+//         console.log('w', ref.current);
+//     }, []);
+
+//     return <FancyButton ref={ref}>Click me!</FancyButton>
+// };
+
+/* 方式2：通过forwardedRef转发, 这种更适用于组件写法 */
+const FancyButton = (props) => {
+    const ref = props.forwardedRef;
     function handleClick() {
         console.log('handleClick', ref.current);
     }
@@ -17,17 +40,22 @@ const FancyButton = React.forwardRef((props, ref) => {
             {props.children}
         </button>
     );
+};
+
+const Wrapper = React.forwardRef((props, ref) => {
+    return <FancyButton forwardedRef={ref} {...props}>Click me!</FancyButton >;
 });
 
 const App = () => {
     const ref = React.createRef();
 
     useEffect(() => {
-        console.log('wrap get ref dom', ref.current);
+        console.log('App', ref.current);
     }, []);
 
-    return <FancyButton ref={ref}>Click me!</FancyButton>
+    return <Wrapper ref={ref} />;
 };
+
 
 ReactDOM.render(
     <App />,

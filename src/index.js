3a68ee0de7195f4babb3228fbb00098f2f73ea91
liuduan <liuduan@58.com>
2020-06-19 15:50:22 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
  * @fileoverview 
  * @author liuduan
  * @Date 2020-06-08 16:02:12
- * @LastEditTime 2020-06-09 16:22:17
+ * @LastEditTime 2020-06-19 15:50:14
  */
-import './forwardRef';
+import './app';

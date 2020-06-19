e3172d3fcfe4a78f9bd966c6a620ff854386d88a
liuduan <liuduan@58.com>
2020-06-19 22:59:25 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * @fileoverview 
  * @author liuduan
  * @Date 2020-06-08 16:02:12
- * @LastEditTime 2020-06-19 15:50:14
+ * @LastEditTime 2020-06-19 20:32:41
  */
-import './app';
+// import './app';
+import './reduxs/store';

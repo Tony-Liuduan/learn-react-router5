ba050d438216b37388af053dd679c210d94ffa3a
liuduan <liuduan@58.com>
2020-06-28 00:22:59 +0800

    add hooks-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-/**
- * @fileoverview 
- * @author liuduan
- * @Date 2020-06-08 16:02:12
- * @LastEditTime 2020-06-20 21:17:02
- */
-import './app';
-// import './reduxs/store';
+import './reduxHooks';
